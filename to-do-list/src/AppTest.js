a8e2da45c8db29cfc6310dcd1db30003ae064d1c
rhyrms <rhyrms@hotmail.com>
2022-07-01 20:41:45 -0300

    Entendendo melhor custom hooks.

---
+++ to-do-list/src/AppTest.js
@@ -0,0 +1,18 @@
+// import { useTest } from './hooks/useTest'
+
+// const AppTest = () => {
+//     const { data: name, refreshData } = useTest()
+//     console.log('AppTest' + '-' + name + (new Date()).getMilliseconds())
+
+//     setTimeout(() => {
+//         refreshData('Mandy')
+//     }, 2000)
+
+//     return (
+//         <div>
+//             {name}
+//         </div>
+//     )
+// }
+
+// export default AppTest
---
+++ to-do-list/src/hooks/useTest.js
@@ -0,0 +1,11 @@
+// import { useState } from "react"
+
+// export const useTest = () => {
+//     const [data, setData] = useState('Naiana')
+
+//     const refreshData = (dataToRefresh) => {
+//         setData(dataToRefresh)
+//     }
+
+//     return { data, refreshData }
+// }
