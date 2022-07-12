aaec990aee9ed9ebc90ff975b59b23ece4ec95fb
rhyrms <rhyrms@hotmail.com>
2022-07-11 23:10:53 -0300

    Projeto fortnite-news removido.

--- fortnite-news/src/pages/About.js
+++
@@ -1,9 +0,0 @@
-const Favorites = () => {
-    return (
-        <div className="page-content">
-            <h1 className="page-title">About Fortnitezin</h1>
-        </div>
-    )
-}
-
-export default Favorites
--- fortnite-news/src/pages/Favorites.js
+++
@@ -1,9 +0,0 @@
-const Favorites = () => {
-    return (
-        <div className="page-content">
-            <h1 className="page-title">Here's what you like...</h1>
-        </div>
-    )
-}
-
-export default Favorites
